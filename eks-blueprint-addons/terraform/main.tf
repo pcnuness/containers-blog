10b9cc91fecc4d562e0689175964891a26788b1f
Paulo César <pcnunes@local>
2025-04-14 22:53:44 -0300

    update application environemnt production

--- eks-blueprint-addons/terraform/main.tf
+++ eks-blueprint-addons/terraform/main.tf
@@ -151,7 +151,7 @@ module "eks_blueprints_kubernetes_addons" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+  version = "~> 5.0"
 
   name = local.name
   cidr = local.vpc_cidr
