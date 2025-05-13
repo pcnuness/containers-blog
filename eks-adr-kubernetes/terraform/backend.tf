e36ce9b225292ff1871d7375463cbcc9b1db8ad5
Paulo César <pcnunes@local>
2025-05-13 15:12:37 -0300

    AWS Controller aws-load-balancer-controller-sa

--- eks-adr-kubernetes/terraform/backend.tf
+++ eks-adr-kubernetes/terraform/backend.tf
@@ -1,14 +1,14 @@
-/*
 terraform {
   backend "s3" {}
 }
-*/
 
+/*
 # EXEC LOCAL
 terraform {
   backend "s3" {
-    bucket         = "730335564649-backend-iac-opentofu"
+    bucket         = "381491977476-backend-iac-opentofu"
     key            = "pcnuness/containers-blog/eks-adr-kubernetes/cluster-kubernetes/terraform.tfstate"
     region         = "us-east-1"
   }
 }
+*/
