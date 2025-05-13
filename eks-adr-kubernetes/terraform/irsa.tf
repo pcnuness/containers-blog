3289b9612f3fffa5809c5d074a27502944ba2645
Paulo César <pcnunes@local>
2025-05-12 23:24:41 -0300

    Terraform Update EKS Deploy - pipe

--- eks-adr-kubernetes/terraform/irsa.tf
+++ eks-adr-kubernetes/terraform/irsa.tf
@@ -12,7 +12,25 @@ module "irsa_aws_lb_controller" {
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller-irsa"]
+    }
+  }
+
+  tags = local.tags
+
+}
+
+module "irsa_aws_lb_controller" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.30.0"
+
+  role_name                              = "cpe-${local.cluster_name}-ingress-nginx-irsa"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider
+      namespace_service_accounts = ["ingress-nginx:ingress-nginx"]
     }
   }
 
