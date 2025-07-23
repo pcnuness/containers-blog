36996f74ccb339847b50230dcd985f9a5bc32a4d
Paulo César Nunes <paulo_cesar_nunes@hotmail.com>
2025-07-23 17:45:19 -0300

    Update irsa.tf

--- eks-adr-kubernetes/terraform/irsa.tf
+++ eks-adr-kubernetes/terraform/irsa.tf
@@ -65,7 +65,7 @@ module "efs_csi_driver_irsa" {
 
 module "crossplane_irsa_aws" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.14"
+  version = "~> 5.54"
 
   role_name_prefix = "${local.cluster_name}-crossplane-irsa"
 
